3741fb05fed3c19202e5ce352402b3f9c57549bb
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-08-01 10:41:55 +0530

    add testimonial

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -1,28 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
-import FlexBetween from "../components/FlexBetween";
-import TestimonidalCard from "../components/TestimonialCard";
+import { Autoplay, Pagination, Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import data from "../components/tesimonialdata";
 import "swiper/css";
 import "swiper/css/pagination";
+import "./form.css";
+import data from "../components/tesimonialdata";
 
-import { Autoplay, Pagination } from "swiper";
-
-const Testimonials = () => {
+const Testimonials = ({ name, feedback }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handleSlideChange = (swiper) => {
+    setCurrentSlide(swiper.activeIndex);
+  };
+
   return (
-    <Box
-      sx={{
-        padding: {
-          xs: "0.5rem 0.5rem 8rem",
-          sm: "0.5rem 0.5rem 8rem",
-          md: "1.5rem 1.5rem 8rem",
-          lg: "3rem 3rem 8rem 8rem",
-        },
-      }}
-    >
+    <Box>
       <Box>
         <Typography
           sx={{
@@ -39,31 +34,78 @@ const Testimonials = () => {
           TESTIMONIALS
         </Typography>
       </Box>
-      <Box className="testimonials">
-        <Swiper
-          autoplay={{
-            delay: 10000,
-            disableOnInteraction: false,
+      <Box
+        sx={{
+          padding: "3rem 3rem",
+          background: "black",
+          textAlign: "center",
+          color: "white",
+        }}
+        id="Gallery"
+      >
+        <Typography
+          sx={{
+            fontSize: {
+              xs: "28px",
+              sm: "32px",
+              md: "36x",
+              lg: "42px",
+            },
           }}
-          grabCursor={true}
-          centeredSlides={true}
-          modules={[Autoplay]}
-          slidesPerView={isMobile ? "1" : "1"}
-          loop={true}
-          /* modules={[EffectCoverflow, Pagination]} */
-          className="mySwiper"
         >
-          {data.map((customer, i) => {
-            return (
-              <SwiperSlide key={customer.id} className="TestimonialSwipe">
-                <TestimonidalCard
-                  feedback={customer.feedback}
-                  name={customer.name}
-                />
+          Our Success Stories
+        </Typography>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            padding: "2rem",
+            paddingLeft: "90px",
+            paddingBottom: "10px",
+            borderRadius: "15px",
+            maxWidth: "580px",
+            maxHeight: "200px",
+            margin: "0 auto",
+            marginTop: "3rem",
+            wordSpacing:"1px",
+            textAlign:"justify",
+            justifyContent:"center",
+            position: "relative",
+            fontFamily:"Georgia, serif"
+          }}
+        >
+          {/* Content for the white card */}
+          <Swiper
+            modules={[Autoplay, Pagination, Navigation]}
+            spaceBetween={50}
+            slidesPerView={1}
+            autoplay={{ delay: 4000 }}
+            pagination={{ clickable: true }}
+            onSlideChange={(swiper) => setCurrentSlide(swiper.activeIndex)} // Add this line
+          >
+            {data.map((customer) => (
+              <SwiperSlide key={customer.id}>
+                <div className="TestimonialSwipe" style={{ color: "black" }}>
+                  {customer.feedback}
+                </div>
               </SwiperSlide>
-            );
-          })}
-        </Swiper>
+            ))}
+          </Swiper>
+          {/* End of Swiper */}
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "-22px",
+              right: "30px",
+            }}
+          >
+            <div style={{ display: "flex" ,justifyContent:"space-between"}}>
+              <div className="arrow-down"></div>
+              <button style={{ backgroundColor: "#E75480", border: "none", color: "white", width: "250px",height:"35px" }}>
+                {data[currentSlide].name}
+              </button>
+            </div>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
